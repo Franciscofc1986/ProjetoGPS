a3fef2a1f1059d791595f5e8e1f8cc4a27b68553
Juliano Costa <julianocs12@hotmail.com>
2016-01-18 20:38:55 -0200

    Inclusão Model

--- Testando/TesteChicao01.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "qualquer coisa";
-echo "testando";
-
-$mensagem = 'Auu xiquinhu, resolve essa treta ai pra nóis kkkk';
-$mensagem .= 'mais é pra resolvê memu em, num fai gambiarra não...';
-?>
